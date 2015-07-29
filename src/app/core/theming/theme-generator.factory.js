5128c9d56068db55fdbd4d1394be569581932cff
srcn <sercanyemen@gmail.com>
2015-07-29 09:28:51 +0300

    theme-generator cleaned-up & reformatted  + removed previous generation methods from theme.module + cleaned up + reformatted

--- src/app/core/theming/theme-generator.factory.js
+++ src/app/core/theming/theme-generator.factory.js
@@ -1,11 +1,13 @@
-(function () {
+(function ()
+{
     'use strict';
 
     angular.module('app.core')
         .factory('themeGenerator', themeGenerator);
 
     /** @ngInject */
-    function themeGenerator(fuseTheming, $rootScope) {
+    function themeGenerator(fuseTheming, $rootScope)
+    {
         var service = {
             generate: generate
         };
@@ -15,36 +17,36 @@
         /**
          * Generate less variables
          */
-        function generate() {
-            console.log(fuseTheming.themes);
-            console.log(fuseTheming.palettes);
-
+        function generate()
+        {
             var palettes = fuseTheming.palettes;
             var themeList = {};
 
             // Iterate through themes
-            angular.forEach(fuseTheming.themes, function (theme) {
+            angular.forEach(fuseTheming.themes, function (theme)
+            {
                 themeList[theme.name] = {};
 
                 // Iterate through color types
-                angular.forEach(theme.colors, function (colorType, colorTypeName) {
+                angular.forEach(theme.colors, function (colorType, colorTypeName)
+                {
                     themeList[theme.name][colorTypeName] = {
-                        'name': colorType.name,
+                        'name'  : colorType.name,
                         'levels': {
                             'default': {
-                                'color': rgba(palettes[colorType.name][colorType.hues.default].value),
+                                'color'   : rgba(palettes[colorType.name][colorType.hues.default].value),
                                 'contrast': rgba(palettes[colorType.name][colorType.hues.default].contrast)
                             },
-                            'hue1': {
-                                'color': rgba(palettes[colorType.name][colorType.hues['hue-1']].value),
+                            'hue1'   : {
+                                'color'   : rgba(palettes[colorType.name][colorType.hues['hue-1']].value),
                                 'contrast': rgba(palettes[colorType.name][colorType.hues['hue-1']].contrast)
                             },
-                            'hue2': {
-                                'color': rgba(palettes[colorType.name][colorType.hues['hue-2']].value),
+                            'hue2'   : {
+                                'color'   : rgba(palettes[colorType.name][colorType.hues['hue-2']].value),
                                 'contrast': rgba(palettes[colorType.name][colorType.hues['hue-2']].contrast)
                             },
-                            'hue3': {
-                                'color': rgba(palettes[colorType.name][colorType.hues['hue-3']].value),
+                            'hue3'   : {
+                                'color'   : rgba(palettes[colorType.name][colorType.hues['hue-3']].value),
                                 'contrast': rgba(palettes[colorType.name][colorType.hues['hue-3']].contrast)
                             }
                         }
@@ -55,8 +57,10 @@
             $rootScope.themes = angular.copy(themeList);
 
             // Reformat themes for Object Usage
-            angular.forEach($rootScope.themes, function (theme) {
-                angular.forEach(theme, function (colorType, colorTypeName) {
+            angular.forEach($rootScope.themes, function (theme)
+            {
+                angular.forEach(theme, function (colorType, colorTypeName)
+                {
                     theme[colorTypeName] = colorType.levels;
                     theme[colorTypeName].color = colorType.levels.default.color;
                     theme[colorTypeName].contrast = colorType.levels.default.contrast;
@@ -65,19 +69,20 @@
             });
             $rootScope.selectedTheme = $rootScope.themes.default;
 
-            console.log($rootScope.selectedTheme);
-
             // Iterate through color list and create less variables
             var lessVars = [];
 
-            angular.forEach(themeList, function (theme, themeName) {
+            angular.forEach(themeList, function (theme, themeName)
+            {
                 lessVars = [];
                 lessVars.push('@themeName:' + themeName);
 
-                angular.forEach(theme, function (colorTypes, colorTypeName) {
-
-                    angular.forEach(colorTypes.levels, function (colors, colorLevelName) {
-                        angular.forEach(colors, function (color, colorName) {
+                angular.forEach(theme, function (colorTypes, colorTypeName)
+                {
+                    angular.forEach(colorTypes.levels, function (colors, colorLevelName)
+                    {
+                        angular.forEach(colors, function (color, colorName)
+                        {
                             lessVars.push('@' + colorTypeName + ucfirst(colorLevelName) + ucfirst(colorName) + ':' + color);
                         });
                     });
@@ -89,19 +94,19 @@
                 // Render less
                 render(lessVars);
             });
-
-            //console.log(lessVars);
-            //console.log(themeList);
         }
 
         /**
          * Convert color array to rgb/rgba
          */
-        function rgba(color) {
-            if (color.length === 3) {
+        function rgba(color)
+        {
+            if ( color.length === 3 )
+            {
                 return 'rgb(' + color.join(',') + ')';
             }
-            else {
+            else
+            {
                 return 'rgba(' + color.join(',') + ')';
             }
         }
@@ -109,33 +114,34 @@
         /**
          * Uppercase first
          */
-        function ucfirst(string) {
+        function ucfirst(string)
+        {
             return string.charAt(0).toUpperCase() + string.slice(1);
         }
 
         /**
          * Render less files
          */
-        function render(lessVars) {
-            var lessColorStyles = '[md-theme=@{themeName}] {\n\n  /**\n   primary \n   **/\n\n  .md-primary-bg {\n    background-color: @primaryDefaultColor;\n    color: @primaryDefaultContrast;\n    &.md-hue-1 {\n      background-color: @primaryHue1Color;\n      color: @primaryHue1Contrast;\n    }\n    &.md-hue-2 {\n      background-color: @primaryHue2Color;\n      color: @primaryHue2Contrast;\n    }\n    &.md-hue-3 {\n      background-color: @primaryHue3Color;\n      color: @primaryHue3Contrast;\n    }\n  }\n\n  .md-primary-fg {\n    color: @primaryDefaultColor;\n    &.md-hue-1 {\n      color: @primaryHue1Color;\n    }\n    &.md-hue-2 {\n      color: @primaryHue2Color;\n    }\n    &.md-hue-3 {\n      color: @primaryHue3Color;\n    }\n  }\n\n  /**\n   accent \n   **/\n\n  .md-accent-bg {\n    background-color: @accentDefaultColor;\n    color: @accentDefaultContrast;\n    &.md-hue-1 {\n      background-color: @accentHue1Color;\n      color: @accentHue1Contrast;\n    }\n    &.md-hue-2 {\n      background-color: @accentHue2Color;\n      color: @accentHue2Contrast;\n    }\n    &.md-hue-3 {\n      background-color: @accentHue3Color;\n      color: @accentHue3Contrast;\n    }\n  }\n\n  .md-accent-fg {\n    color: @accentDefaultColor;\n    &.md-hue-1 {\n      color: @accentHue1Color;\n    }\n    &.md-hue-2 {\n      color: @accentHue2Color;\n    }\n    &.md-hue-3 {\n      color: @accentHue3Color;\n    }\n  }\n\n  /**\n   warn \n   **/\n\n  .md-warn-bg {\n    background-color: @warnDefaultColor;\n    color: @warnDefaultContrast;\n    &.md-hue-1 {\n      background-color: @warnHue1Color;\n      color: @warnHue1Contrast;\n    }\n    &.md-hue-2 {\n      background-color: @warnHue2Color;\n      color: @warnHue2Contrast;\n    }\n    &.md-hue-3 {\n      background-color: @warnHue3Color;\n      color: @warnHue3Contrast;\n    }\n  }\n\n  .md-warn-fg {\n    color: @warnDefaultColor;\n    &.md-hue-1 {\n      color: @warnHue1Color;\n    }\n    &.md-hue-2 {\n      color: @warnHue2Color;\n    }\n    &.md-hue-3 {\n      color: @warnHue3Color;\n    }\n  }\n\n  /**\n   background \n   **/\n\n  .md-background-bg {\n    background-color: @backgroundDefaultColor;\n    color: @backgroundDefaultContrast;\n    &.md-hue-1 {\n      background-color: @backgroundHue1Color;\n      color: @backgroundHue1Contrast;\n    }\n    &.md-hue-2 {\n      background-color: @backgroundHue2Color;\n      color: @backgroundHue2Contrast;\n    }\n    &.md-hue-3 {\n      background-color: @backgroundHue3Color;\n      color: @backgroundHue3Contrast;\n    }\n  }\n\n  .md-background-fg {\n    color: @backgroundDefaultColor;\n    &.md-hue-1 {\n      color: @backgroundHue1Color;\n    }\n    &.md-hue-2 {\n      color: @backgroundHue2Color;\n    }\n    &.md-hue-3 {\n      color: @backgroundHue3Color;\n    }\n  }\n}\n';
+        function render(lessVars)
+        {
+            var lessColorStyles = '[md-theme=@{themeName}] {\n\n    /**\n    * Primary\n    */\n    .md-primary-bg {\n        background-color: @primaryDefaultColor;\n        color: @primaryDefaultContrast;\n        &.md-hue-1 {\n            background-color: @primaryHue1Color;\n            color: @primaryHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @primaryHue2Color;\n            color: @primaryHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @primaryHue3Color;\n            color: @primaryHue3Contrast;\n        }\n    }\n\n    .md-primary-fg {\n        color: @primaryDefaultColor;\n        &.md-hue-1 {\n            color: @primaryHue1Color;\n        }\n        &.md-hue-2 {\n            color: @primaryHue2Color;\n        }\n        &.md-hue-3 {\n            color: @primaryHue3Color;\n        }\n    }\n\n    /**\n      * Accent\n      */\n    .md-accent-bg {\n        background-color: @accentDefaultColor;\n        color: @accentDefaultContrast;\n        &.md-hue-1 {\n            background-color: @accentHue1Color;\n            color: @accentHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @accentHue2Color;\n            color: @accentHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @accentHue3Color;\n            color: @accentHue3Contrast;\n        }\n    }\n\n    .md-accent-fg {\n        color: @accentDefaultColor;\n        &.md-hue-1 {\n            color: @accentHue1Color;\n        }\n        &.md-hue-2 {\n            color: @accentHue2Color;\n        }\n        &.md-hue-3 {\n            color: @accentHue3Color;\n        }\n    }\n\n    /**\n      * Warn\n      */\n    .md-warn-bg {\n        background-color: @warnDefaultColor;\n        color: @warnDefaultContrast;\n        &.md-hue-1 {\n            background-color: @warnHue1Color;\n            color: @warnHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @warnHue2Color;\n            color: @warnHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @warnHue3Color;\n            color: @warnHue3Contrast;\n        }\n    }\n\n    .md-warn-fg {\n        color: @warnDefaultColor;\n        &.md-hue-1 {\n            color: @warnHue1Color;\n        }\n        &.md-hue-2 {\n            color: @warnHue2Color;\n        }\n        &.md-hue-3 {\n            color: @warnHue3Color;\n        }\n    }\n\n    /**\n    * Background\n    */\n    .md-background-bg {\n        background-color: @backgroundDefaultColor;\n        color: @backgroundDefaultContrast;\n        &.md-hue-1 {\n            background-color: @backgroundHue1Color;\n            color: @backgroundHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @backgroundHue2Color;\n            color: @backgroundHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @backgroundHue3Color;\n            color: @backgroundHue3Contrast;\n        }\n    }\n\n    .md-background-fg {\n        color: @backgroundDefaultColor;\n        &.md-hue-1 {\n            color: @backgroundHue1Color;\n        }\n        &.md-hue-2 {\n            color: @backgroundHue2Color;\n        }\n        &.md-hue-3 {\n            color: @backgroundHue3Color;\n        }\n    }\n}\n';
             var lessInput = lessVars + lessColorStyles;
-
-            console.log(lessInput);
 
             less
                 .render(lessInput, {})
-                .then(function (output) {
+                .then(function (output)
+                {
                     var headEl = document.getElementsByTagName('head')[0];
                     var styleEl = document.createElement('style');
                     styleEl.setAttribute('type', 'text/css');
                     styleEl.appendChild(document.createTextNode(output.css));
                     headEl.appendChild(styleEl);
                 },
-                function (error) {
+                function (error)
+                {
                     console.error(error);
                 });
         }
     }
 
-})
-();
+})();
--- src/app/core/theming/theme.module.js
+++ src/app/core/theming/theme.module.js
@@ -11,15 +11,10 @@
     /** @ngInject */
     function fuseTheming()
     {
-        var parser,
-            palettes,
+        var palettes,
             themes;
 
         return {
-            setParser  : function (_parser)
-            {
-                parser = _parser;
-            },
             setPalettes: function (_palettes)
             {
                 palettes = _palettes;
@@ -31,7 +26,6 @@
             $get       : function ()
             {
                 return {
-                    parser  : parser,
                     palettes: palettes,
                     themes  : themes
                 };
@@ -62,71 +56,17 @@
                 .backgroundPalette(theme.background.name, theme.background.options);
         }
 
-        fuseThemingProvider.setParser($mdThemingProvider._parseRules);
+        // Store certain generated PALETTES and THEMES objects from
+        // $mdThemingProvider so we can inject them into other areas
         fuseThemingProvider.setPalettes($mdThemingProvider._PALETTES);
         fuseThemingProvider.setThemes($mdThemingProvider._THEMES);
     }
 
     /** @ngInject */
-    function runBlock($rootScope, fuseTheming, fuseThemes, $mdColorPalette, themeGenerator)
+    function runBlock(themeGenerator)
     {
-
-        // Generate custom rules
-        //generateCustomRules();
-
-        // Generate theme palette css
-        //themeService.generateThemePaletteCss(fuseThemes.default);
-
-        //console.log($rootScope);
-
-        /**
-         * Generate custom css rules
-         */
-        function generateCustomRules()
-        {
-            // Get document head
-            var headEl = document.getElementsByTagName('head')[0];
-
-            // Custom rules
-            var customRules = {
-                'primary': ".md-primary-bg.md-THEME_NAME-theme .secondary-text { color: '{{primary-contrast}}' }",
-                //'accent' : ".md-THEME_NAME-theme.md-accent-bg .secondary-text { color: '{{accent-contrast}}' }",
-                //'warn'   : ".md-THEME_NAME-theme.md-warn-bg .secondary-text { color: '{{warn-contrast}}' }"
-                //'background': ".md-THEME_NAME-theme .secondary-text { color: '{{foreground-2}}' }",
-            };
-
-            // Iterate through each theme
-            for ( var themeName in fuseThemes )
-            {
-                // Get the theme
-                var theme = fuseTheming.themes[themeName];
-
-                // Iterate through every color type
-                for ( var colorType in customRules )
-                {
-                    // Generate rules for the selected color type
-                    var generatedRules = fuseTheming.parser(theme, colorType, customRules[colorType]);
-
-                    // Iterate through generated rules
-                    angular.forEach(generatedRules, function (generatedRule)
-                    {
-                        // Create a new style element
-                        var styleEl = document.createElement('style');
-                        styleEl.setAttribute('type', 'text/css');
-
-                        // Insert generated rules to the style element and append the style element to the <head>
-                        styleEl.appendChild(document.createTextNode(generatedRule));
-                        headEl.appendChild(styleEl);
-                    });
-                }
-
-                //console.log(generatedRules);
-                //console.log(fuseTheming.themes[themeName]);
-
-            }
-        }
-
-        // Generate themes
+        // Generate extra classes based on registered themes so we
+        // can use same colors with non-angular-material elements
         themeGenerator.generate();
     }
 })();
