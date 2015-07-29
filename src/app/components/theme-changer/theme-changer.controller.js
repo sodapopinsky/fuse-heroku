8d6249bedce83946763645fc188743ae40a934c9
srcn <sercanyemen@gmail.com>
2015-07-29 09:27:54 +0300

    theme-changer cleaned up + re-formattted

--- src/app/components/theme-changer/theme-changer.controller.js
+++ src/app/components/theme-changer/theme-changer.controller.js
@@ -1,33 +1,50 @@
-(function () {
+(function ()
+{
     'use strict';
 
     angular.module('app.core')
         .controller('ThemeChangerController', ThemeChangerController);
 
     /** @ngInject */
-    function ThemeChangerController($rootScope, $route, fuseThemes, fuseTheming) {
+    function ThemeChangerController($rootScope, $route, fuseThemes, fuseTheming)
+    {
         var vm = this;
         vm.isOpen = false;
         vm.setTheme = setTheme;
         vm.themes = fuseThemes;
         vm.getThemeColor = getThemeColor;
 
-        function setTheme(themeName, themePalette) {
+        /**
+         * Set theme
+         *
+         * @param themeName
+         */
+        function setTheme(themeName)
+        {
             $rootScope.appTheme = themeName;
             $rootScope.selectedTheme = $rootScope.themes[themeName];
-            //themeService.generateThemePaletteCss(themePalette);
-            //for update chart colors
             $route.reload();
         }
 
-        function getThemeColor(paletteColor, level, op) {
-            op = op || 1;
-            var rgb = angular.copy(fuseTheming.palettes[paletteColor.name][level].value);
-            rgb.push(op);
-            //console.log(rgb);
+        /**
+         * Get certain color from theme
+         *
+         * @param paletteColor
+         * @param level
+         * @returns {string}
+         */
+        function getThemeColor(paletteColor, level)
+        {
+            var color = angular.copy(fuseTheming.palettes[paletteColor.name][level].value);
 
-            return 'rgba(' + rgb.join() + ')';
+            if ( color.length === 4 )
+            {
+                return 'rgba(' + color.join() + ')';
+            }
+            else
+            {
+                return 'rgb(' + color.join() + ')';
+            }
         }
-
     }
 })();
