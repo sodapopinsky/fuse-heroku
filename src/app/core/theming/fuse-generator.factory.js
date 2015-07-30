c0d4f257634ac321ea3037032f067b0567489fc8
srcn <sercanyemen@gmail.com>
2015-07-30 11:21:23 +0300

    added text color levels to the generator

--- src/app/core/theming/fuse-generator.factory.js
+++ src/app/core/theming/fuse-generator.factory.js
@@ -12,7 +12,7 @@
         var themes = {};
 
         var service = {
-            generate     : generate
+            generate: generate
         };
 
         return service;
@@ -41,25 +41,39 @@
                         'name'  : colorType.name,
                         'levels': {
                             'default': {
-                                'color'   : rgba(registeredPalettes[colorType.name][colorType.hues.default].value),
-                                'contrast': rgba(registeredPalettes[colorType.name][colorType.hues.default].contrast)
+                                'color'    : rgba(registeredPalettes[colorType.name][colorType.hues.default].value),
+                                'contrast1' : rgba(registeredPalettes[colorType.name][colorType.hues.default].contrast, 1),
+                                'contrast2': rgba(registeredPalettes[colorType.name][colorType.hues.default].contrast, 2),
+                                'contrast3': rgba(registeredPalettes[colorType.name][colorType.hues.default].contrast, 3),
+                                'contrast4': rgba(registeredPalettes[colorType.name][colorType.hues.default].contrast, 4)
                             },
                             'hue1'   : {
                                 'color'   : rgba(registeredPalettes[colorType.name][colorType.hues['hue-1']].value),
-                                'contrast': rgba(registeredPalettes[colorType.name][colorType.hues['hue-1']].contrast)
+                                'contrast1': rgba(registeredPalettes[colorType.name][colorType.hues['hue-1']].contrast, 1),
+                                'contrast2': rgba(registeredPalettes[colorType.name][colorType.hues['hue-1']].contrast, 2),
+                                'contrast3': rgba(registeredPalettes[colorType.name][colorType.hues['hue-1']].contrast, 3),
+                                'contrast4': rgba(registeredPalettes[colorType.name][colorType.hues['hue-1']].contrast, 4)
                             },
                             'hue2'   : {
                                 'color'   : rgba(registeredPalettes[colorType.name][colorType.hues['hue-2']].value),
-                                'contrast': rgba(registeredPalettes[colorType.name][colorType.hues['hue-2']].contrast)
+                                'contrast1': rgba(registeredPalettes[colorType.name][colorType.hues['hue-2']].contrast, 1),
+                                'contrast2': rgba(registeredPalettes[colorType.name][colorType.hues['hue-2']].contrast, 2),
+                                'contrast3': rgba(registeredPalettes[colorType.name][colorType.hues['hue-2']].contrast, 3),
+                                'contrast4': rgba(registeredPalettes[colorType.name][colorType.hues['hue-2']].contrast, 4)
                             },
                             'hue3'   : {
                                 'color'   : rgba(registeredPalettes[colorType.name][colorType.hues['hue-3']].value),
-                                'contrast': rgba(registeredPalettes[colorType.name][colorType.hues['hue-3']].contrast)
+                                'contrast1': rgba(registeredPalettes[colorType.name][colorType.hues['hue-3']].contrast, 1),
+                                'contrast2': rgba(registeredPalettes[colorType.name][colorType.hues['hue-3']].contrast, 2),
+                                'contrast3': rgba(registeredPalettes[colorType.name][colorType.hues['hue-3']].contrast, 3),
+                                'contrast4': rgba(registeredPalettes[colorType.name][colorType.hues['hue-3']].contrast, 4)
                             }
                         }
                     };
                 });
             });
+
+            console.log(themes);
 
             // Store the themes in the service for external use
             storeThemes(themes);
@@ -98,7 +112,7 @@
 
         /**
          * Modify and store themes for global use
-         * 
+         *
          * @param themes
          */
         function storeThemes(themes)
@@ -112,7 +126,10 @@
                 {
                     theme[colorTypeName] = colorType.levels;
                     theme[colorTypeName].color = colorType.levels.default.color;
-                    theme[colorTypeName].contrast = colorType.levels.default.contrast;
+                    theme[colorTypeName].contrast1 = colorType.levels.default.contrast1;
+                    theme[colorTypeName].contrast2 = colorType.levels.default.contrast2;
+                    theme[colorTypeName].contrast3 = colorType.levels.default.contrast3;
+                    theme[colorTypeName].contrast4 = colorType.levels.default.contrast4;
                     delete theme[colorTypeName].default;
                 });
             });
@@ -120,6 +137,8 @@
             // Store themes and selected theme on the $rootScope
             $rootScope.themes = t;
             $rootScope.selectedTheme = t.default;
+
+            console.log(t);
         }
 
 
@@ -128,7 +147,7 @@
          */
         function render(lessVars)
         {
-            var lessColorStyles = '[md-theme=@{themeName}] {\n\n    /**\n    * Primary\n    */\n    .md-primary-bg {\n        background-color: @primaryDefaultColor;\n        color: @primaryDefaultContrast;\n        &.md-hue-1 {\n            background-color: @primaryHue1Color;\n            color: @primaryHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @primaryHue2Color;\n            color: @primaryHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @primaryHue3Color;\n            color: @primaryHue3Contrast;\n        }\n    }\n\n    .md-primary-fg {\n        color: @primaryDefaultColor;\n        &.md-hue-1 {\n            color: @primaryHue1Color;\n        }\n        &.md-hue-2 {\n            color: @primaryHue2Color;\n        }\n        &.md-hue-3 {\n            color: @primaryHue3Color;\n        }\n    }\n\n    /**\n      * Accent\n      */\n    .md-accent-bg {\n        background-color: @accentDefaultColor;\n        color: @accentDefaultContrast;\n        &.md-hue-1 {\n            background-color: @accentHue1Color;\n            color: @accentHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @accentHue2Color;\n            color: @accentHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @accentHue3Color;\n            color: @accentHue3Contrast;\n        }\n    }\n\n    .md-accent-fg {\n        color: @accentDefaultColor;\n        &.md-hue-1 {\n            color: @accentHue1Color;\n        }\n        &.md-hue-2 {\n            color: @accentHue2Color;\n        }\n        &.md-hue-3 {\n            color: @accentHue3Color;\n        }\n    }\n\n    /**\n      * Warn\n      */\n    .md-warn-bg {\n        background-color: @warnDefaultColor;\n        color: @warnDefaultContrast;\n        &.md-hue-1 {\n            background-color: @warnHue1Color;\n            color: @warnHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @warnHue2Color;\n            color: @warnHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @warnHue3Color;\n            color: @warnHue3Contrast;\n        }\n    }\n\n    .md-warn-fg {\n        color: @warnDefaultColor;\n        &.md-hue-1 {\n            color: @warnHue1Color;\n        }\n        &.md-hue-2 {\n            color: @warnHue2Color;\n        }\n        &.md-hue-3 {\n            color: @warnHue3Color;\n        }\n    }\n\n    /**\n    * Background\n    */\n    .md-background-bg {\n        background-color: @backgroundDefaultColor;\n        color: @backgroundDefaultContrast;\n        &.md-hue-1 {\n            background-color: @backgroundHue1Color;\n            color: @backgroundHue1Contrast;\n        }\n        &.md-hue-2 {\n            background-color: @backgroundHue2Color;\n            color: @backgroundHue2Contrast;\n        }\n        &.md-hue-3 {\n            background-color: @backgroundHue3Color;\n            color: @backgroundHue3Contrast;\n        }\n    }\n\n    .md-background-fg {\n        color: @backgroundDefaultColor;\n        &.md-hue-1 {\n            color: @backgroundHue1Color;\n        }\n        &.md-hue-2 {\n            color: @backgroundHue2Color;\n        }\n        &.md-hue-3 {\n            color: @backgroundHue3Color;\n        }\n    }\n}\n';
+            var lessColorStyles = '[md-theme=@{themeName}] {\n\n    .secondary-text,\n    .icon {\n        color: @backgroundDefaultContrast2;\n    }\n\n    .hint-text,\n    .disabled-text {\n        color: @backgroundDefaultContrast3;\n    }\n\n    .divider {\n        color: @backgroundDefaultContrast4;\n    }\n\n    // Primary\n    .md-primary-bg {\n        background-color: @primaryDefaultColor;\n        color: @primaryDefaultContrast1;\n\n        .secondary-text,\n        .icon {\n            color: @primaryDefaultContrast2;\n        }\n\n        .hint-text,\n        .disabled-text {\n            color: @primaryDefaultContrast3;\n        }\n\n        .divider {\n            color: @primaryDefaultContrast4;\n        }\n\n        // Primary, Hue-1\n        &.md-hue-1 {\n            background-color: @primaryHue1Color;\n            color: @primaryHue1Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @primaryHue1Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @primaryHue1Contrast3;\n            }\n\n            .divider {\n                color: @primaryHue1Contrast4;\n            }\n        }\n\n        // Primary, Hue-2\n        &.md-hue-2 {\n            background-color: @primaryHue2Color;\n            color: @primaryHue2Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @primaryHue2Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @primaryHue2Contrast3;\n            }\n\n            .divider {\n                color: @primaryHue2Contrast4;\n            }\n        }\n\n        // Primary, Hue-3\n        &.md-hue-3 {\n            background-color: @primaryHue3Color;\n            color: @primaryHue3Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @primaryHue3Contrast1;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @primaryHue3Contrast3;\n            }\n\n            .divider {\n                color: @primaryHue3Contrast4;\n            }\n        }\n    }\n\n    // Primary foreground\n    .md-primary-fg {\n        color: @primaryDefaultColor;\n\n        // Primary foreground, Hue-1\n        &.md-hue-1 {\n            color: @primaryHue1Color;\n        }\n\n        // Primary foreground, Hue-2\n        &.md-hue-2 {\n            color: @primaryHue2Color;\n        }\n\n        // Primary foreground, Hue-3\n        &.md-hue-3 {\n            color: @primaryHue3Color;\n        }\n    }\n\n    // Accent\n    .md-accent-bg {\n        background-color: @accentDefaultColor;\n        color: @accentDefaultContrast1;\n\n        .secondary-text,\n        .icon {\n            color: @accentDefaultContrast2;\n        }\n\n        .hint-text,\n        .disabled-text {\n            color: @accentDefaultContrast3;\n        }\n\n        .divider {\n            color: @accentDefaultContrast4;\n        }\n\n        // Accent, Hue-1\n        &.md-hue-1 {\n            background-color: @accentHue1Color;\n            color: @accentHue1Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @accentHue1Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @accentHue1Contrast3;\n            }\n\n            .divider {\n                color: @accentHue1Contrast4;\n            }\n        }\n\n        // Accent, Hue-2\n        &.md-hue-2 {\n            background-color: @accentHue2Color;\n            color: @accentHue2Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @accentHue2Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @accentHue2Contrast3;\n            }\n\n            .divider {\n                color: @accentHue2Contrast4;\n            }\n        }\n\n        // Accent, Hue-3\n        &.md-hue-3 {\n            background-color: @accentHue3Color;\n            color: @accentHue3Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @accentHue3Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @accentHue3Contrast3;\n            }\n\n            .divider {\n                color: @accentHue3Contrast4;\n            }\n        }\n    }\n\n    // Accent foreground\n    .md-accent-fg {\n        color: @accentDefaultColor;\n        \n        // Accent foreground, Hue-1\n        &.md-hue-1 {\n            color: @accentHue1Color;\n        }\n        \n        // Accent foreground, Hue-2\n        &.md-hue-2 {\n            color: @accentHue2Color;\n        }\n        \n        // Accent foreground, Hue-3\n        &.md-hue-3 {\n            color: @accentHue3Color;\n        }\n    }\n\n    // Warn\n    .md-warn-bg {\n        background-color: @warnDefaultColor;\n        color: @warnDefaultContrast1;\n\n        .secondary-text,\n        .icon {\n            color: @warnDefaultContrast2;\n        }\n\n        .hint-text,\n        .disabled-text {\n            color: @warnDefaultContrast3;\n        }\n\n        .divider {\n            color: @warnDefaultContrast4;\n        }\n        \n        // Warn, Hue-1\n        &.md-hue-1 {\n            background-color: @warnHue1Color;\n            color: @warnHue1Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @warnHue1Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @warnHue1Contrast3;\n            }\n\n            .divider {\n                color: @warnHue1Contrast4;\n            }\n        }\n\n        // Warn, Hue-2\n        &.md-hue-2 {\n            background-color: @warnHue2Color;\n            color: @warnHue2Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @warnHue2Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @warnHue2Contrast3;\n            }\n\n            .divider {\n                color: @warnHue2Contrast4;\n            }\n        }\n\n        // Warn, Hue-3\n        &.md-hue-3 {\n            background-color: @warnHue3Color;\n            color: @warnHue3Contrast1;\n\n            .secondary-text,\n            .icon {\n                color: @warnHue3Contrast2;\n            }\n\n            .hint-text,\n            .disabled-text {\n                color: @warnHue3Contrast3;\n            }\n\n            .divider {\n                color: @warnHue3Contrast4;\n            }\n        }\n    }\n\n    // Warn foreground\n    .md-warn-fg {\n        color: @warnDefaultColor;\n        \n        // Warn foreground, Hue-1\n        &.md-hue-1 {\n            color: @warnHue1Color;\n        }\n        \n        // Warn foreground, Hue-2\n        &.md-hue-2 {\n            color: @warnHue2Color;\n        }\n        \n        // Warn foreground, Hue-3\n        &.md-hue-3 {\n            color: @warnHue3Color;\n        }\n    }\n\n    // Background\n    .md-background-bg {\n        background-color: @backgroundDefaultColor;\n        color: @backgroundDefaultContrast1;\n        \n        // Background, Hue-1\n        &.md-hue-1 {\n            background-color: @backgroundHue1Color;\n            color: @backgroundHue1Contrast1;\n        }\n        \n        // Background, Hue-2\n        &.md-hue-2 {\n            background-color: @backgroundHue2Color;\n            color: @backgroundHue2Contrast1;\n        }\n        \n        // Background, Hue-3\n        &.md-hue-3 {\n            background-color: @backgroundHue3Color;\n            color: @backgroundHue3Contrast1;\n        }\n    }\n\n    // Background foreground\n    .md-background-fg {\n        color: @backgroundDefaultColor;\n        \n        // Background foreground, Hue-1\n        &.md-hue-1 {\n            color: @backgroundHue1Color;\n        }\n        \n        // Background foreground, Hue-2\n        &.md-hue-2 {\n            color: @backgroundHue2Color;\n        }\n        \n        // Background foreground, Hue-3\n        &.md-hue-3 {\n            color: @backgroundHue3Color;\n        }\n    }\n}\n';
             var lessInput = lessVars + lessColorStyles;
 
             less
@@ -149,17 +168,72 @@
 
         /**
          * Convert color array to rgb/rgba
+         * Also apply contrast fixes if necessary
          */
-        function rgba(color)
+        function rgba(color, _contrastLevel)
         {
+            var contrastLevel = _contrastLevel || false;
+
+            // Fix 255,255,255,0.XX to 255,255,255
+            if ( color.length === 4 && color[0] === 255)
+            {
+                color.splice(3, 4);
+            }
+
+            if ( contrastLevel )
+            {
+                color = applyContrast(color, contrastLevel);
+            }
+
             if ( color.length === 3 )
             {
                 return 'rgb(' + color.join(',') + ')';
             }
-            else
+            else if ( color.length === 4 )
             {
                 return 'rgba(' + color.join(',') + ')';
             }
+            else
+            {
+                console.error('Invalid number of arguments in the color array: ' + color.length + '\n' + 'The array must have 3 or 4 colors.');
+            }
+        }
+
+        /**
+         * Apply given contrast level to the given color
+         *
+         * @param color
+         * @param contrastLevel
+         */
+        function applyContrast(color, contrastLevel)
+        {
+            var contrastLevels = {
+                'white': {
+                    '1': '1',
+                    '2': '0.7',
+                    '3': '0.3',
+                    '4': '0.12'
+                },
+                'black': {
+                    '1': '0.87',
+                    '2': '0.54',
+                    '3': '0.26',
+                    '4': '0.12'
+                }
+            };
+
+            // If white
+            if ( color[0] === 255 && color[1] === 255 && color[2] === 255)
+            {
+                color[3] = contrastLevels.white[contrastLevel];
+            }
+            // If black
+            else if ( color[0] === 0 && color[1] === 0, color[2] === 0 )
+            {
+                color[3] = contrastLevels.black[contrastLevel];
+            }
+
+            return color;
         }
 
         /**
