97a02782ac32513ec216bf2175d7b89792fce05f
srcn <sercanyemen@gmail.com>
2015-08-07 17:23:57 +0300

    working on left-pane layout style

--- src/app/main/ui/layouts/style-1/leftpane/leftpane.controller.js
+++ src/app/main/ui/layouts/style-1/leftpane/leftpane.controller.js
@@ -6,14 +6,25 @@
         .controller('Style1LeftpaneController', Style1LeftpaneController);
 
     /** @ngInject */
-    function Style1LeftpaneController()
+    function Style1LeftpaneController($mdSidenav)
     {
         var vm = this;
 
         // Data
 
         // Methods
+        vm.toggleSidenav = toggleSidenav;
 
         //////////
+
+        /**
+         * Toggle sidenav
+         *
+         * @param sidenavId
+         */
+        function toggleSidenav(sidenavId)
+        {
+            $mdSidenav(sidenavId).toggle();
+        }
     }
 })();
