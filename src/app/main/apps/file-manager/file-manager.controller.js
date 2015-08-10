1fa035cd30514c17a938112b6778ed36f6c3082a
srcn <sercanyemen@gmail.com>
2015-08-10 13:24:48 +0300

    pane ids for consistency

--- src/app/main/apps/file-manager/file-manager.controller.js
+++ src/app/main/apps/file-manager/file-manager.controller.js
@@ -52,7 +52,7 @@
         function toggleDetails(item)
         {
             vm.selected = item;
-            toggleSidenav('details-sidenav');
+            toggleSidenav('rightpane');
         }
 
         /**
