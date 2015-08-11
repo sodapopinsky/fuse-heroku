06e77b5b365ad212f4fdadb7507b9f870971f631
srcn <sercanyemen@gmail.com>
2015-08-11 13:09:24 +0300

    moved chart pages into the fullwidth layout + removed empty chart library specific less files, we can add them later if needed

--- src/app/main/components/charts/c3/c3.controller.js
+++ src/app/main/components/charts/c3/c3.controller.js
@@ -1,15 +1,18 @@
-(function () {
+(function ()
+{
     'use strict';
 
     angular
         .module('app.components')
         .controller('C3Controller', C3Controller);
 
-    function C3Controller(fuseTheming) {
+    function C3Controller(fuseTheming)
+    {
         var vm = this;
 
         // Data
         vm.themes = fuseTheming.themes;
+
         // Methods
 
         //////////
