001eb9455e2124a86a81fc1f91a976ac9360d826
srcn <sercanyemen@gmail.com>
2015-08-14 08:26:48 +0300

    ms-scroll directive for perfectScrollbar + .scrollable helper class as an md-content alternative

---
+++ src/app/core/directives/ms-scroll/ms-scroll.directive.js
@@ -0,0 +1,79 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('fuse')
+        .directive('msScroll', msScroll);
+
+    /** @ngInject */
+    function msScroll($timeout)
+    {
+        return {
+            restrict: 'AE',
+            compile : function (tElement)
+            {
+                // Do not replace scrollbars on mobile devices
+                var md = new MobileDetect(window.navigator.userAgent);
+
+                if ( md.mobile() )
+                {
+                    return false;
+                }
+
+                // Add class
+                tElement.addClass('ms-scroll');
+
+                return function postLink($scope, $element, $attrs)
+                {
+                    var options = {};
+
+                    // If options supplied, evaluate the given
+                    // value. This is because we don't want to
+                    // have an isolated scope but still be able
+                    // to use scope variables.
+                    // We don't want an isolated scope because
+                    // we should be able to use this everywhere
+                    // especially with other directives
+                    if ( $attrs.msScroll )
+                    {
+                        options = $scope.$eval($attrs.msScroll);
+                    }
+
+                    // Initialize the perfectScrollbar
+                    $timeout(function ()
+                    {
+                        $element.perfectScrollbar(options);
+                    }, 0);
+
+                    // Watch scrollHeight and update
+                    // the scrollbar if it changes
+                    $scope.$watch(function ()
+                    {
+                        return $element.prop('scrollHeight');
+                    }, function (current, old)
+                    {
+                        if ( angular.isUndefined(current) || angular.equals(current, old) )
+                        {
+                            return;
+                        }
+
+                        updateScrollbar();
+                    });
+
+                    // Update the scrollbar
+                    function updateScrollbar()
+                    {
+                        $element.perfectScrollbar('update');
+                    }
+
+                    // Cleanup on $destroy
+                    $scope.$on('$destroy', function ()
+                    {
+                        $element.off('mouseenter');
+                        $element.perfectScrollbar('destroy');
+                    });
+                };
+            }
+        };
+    }
+})();
