3b260a8c2049134c247f3cd727c238a0a63d2e40
mustafahlvc <mustafahlvc@gmail.com>
2015-08-16 12:51:43 +0300

    api service structure revised.

--- src/app/core/services/api/api.service.js
+++ src/app/core/services/api/api.service.js
@@ -9,24 +9,24 @@
     function apiService($resource)
     {
         var api = {};
-        api.url = 'app/core/services/api/';
+        api.dataUrl = 'app/core/services/api/data/';
 
-        api.contacts = $resource(api.url + 'notifications/contacts.json', null,
+        api.contacts = $resource(api.dataUrl + 'notifications/contacts.json', null,
             {
                 'get': {method: 'get'}
             });
 
-        api.events = $resource(api.url + 'notifications/events.json', null,
+        api.events = $resource(api.dataUrl + 'notifications/events.json', null,
             {
                 'get': {method: 'get'}
             });
 
-        api.notes = $resource(api.url + 'notifications/notes.json', null,
+        api.notes = $resource(api.dataUrl + 'notifications/notes.json', null,
             {
                 'get': {method: 'get'}
             });
 
-        api.activities = $resource(api.url + 'notifications/activities.json', null,
+        api.activities = $resource(api.dataUrl + 'notifications/activities.json', null,
             {
                 'get': {method: 'get'}
             });
@@ -37,18 +37,18 @@
             });
 
         api.mail = {
-            inbox: $resource(api.url + 'mail/inbox.json', null,
+            inbox: $resource(api.dataUrl + 'mail/inbox.json', null,
                 {
                     'get': {method: 'get'}
                 })
         };
 
         api.todo = {
-            tasks: $resource(api.url + 'todo/tasks.json', null,
+            tasks: $resource(api.dataUrl + 'todo/tasks.json', null,
                 {
                     'get': {method: 'get'}
                 }),
-            tags : $resource(api.url + 'todo/tags.json', null,
+            tags : $resource(api.dataUrl + 'todo/tags.json', null,
                 {
                     'get': {method: 'get'}
                 })
@@ -56,65 +56,65 @@
 
         api.profile = {
             timeline    : {
-                posts: $resource(api.url + 'profile/timeline/posts.json', null,
+                posts: $resource(api.dataUrl + 'profile/timeline/posts.json', null,
                     {
                         'get': {method: 'get'}
                     }),
 
-                activities: $resource(api.url + 'profile/timeline/activities.json', null,
+                activities: $resource(api.dataUrl + 'profile/timeline/activities.json', null,
                     {
                         'get': {method: 'get'}
                     })
             },
-            about       : $resource(api.url + 'profile/about/about.json', null,
+            about       : $resource(api.dataUrl + 'profile/about/about.json', null,
                 {
                     'get': {method: 'get'}
                 }),
-            photosVideos: $resource(api.url + 'profile/photos-videos/photos-videos.json', null,
+            photosVideos: $resource(api.dataUrl + 'profile/photos-videos/photos-videos.json', null,
                 {
                     'get': {method: 'get'}
                 })
         };
 
         api.search = {
-            classic : $resource(api.url + 'search/classic.json', null,
+            classic : $resource(api.dataUrl + 'search/classic.json', null,
                 {
                     'get': {method: 'get'}
                 }),
-            mails   : $resource(api.url + 'search/mails.json', null,
+            mails   : $resource(api.dataUrl + 'search/mails.json', null,
                 {
                     'get': {method: 'get'}
                 }),
-            users   : $resource(api.url + 'search/users.json', null,
+            users   : $resource(api.dataUrl + 'search/users.json', null,
                 {
                     'get': {method: 'get'}
                 }),
-            contacts: $resource(api.url + 'search/contacts.json', null,
+            contacts: $resource(api.dataUrl + 'search/contacts.json', null,
                 {
                     'get': {method: 'get'}
                 })
         };
 
         api.fileManager = {
-            documents: $resource(api.url + 'file-manager/documents.json', null,
+            documents: $resource(api.dataUrl + 'file-manager/documents.json', null,
                 {
                     'get': {method: 'get'}
                 })
         };
 
-        api.invoice = $resource(api.url + 'invoice/invoice.json', null,
+        api.invoice = $resource(api.dataUrl + 'invoice/invoice.json', null,
             {
                 'get': {method: 'get'}
             });
 
         api.tables = {
-            employees: $resource(api.url + 'tables/employees.json', null,
+            employees: $resource(api.dataUrl + 'tables/employees.json', null,
                 {
                     'get': {method: 'get'}
                 })
         };
 
-        api.timeline = $resource(api.url + 'timeline/timeline.json', null,
+        api.timeline = $resource(api.dataUrl + 'timeline/timeline.json', null,
             {
                 'get': {method: 'get'}
             });
