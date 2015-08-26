cd3307cab1614f1d5b96c58e7d052d56598d47b2
srcn <sercanyemen@gmail.com>
2015-08-26 10:14:01 +0300

    renamed random class directive + some tweaks

---
+++ src/app/core/directives/ms-random-class/ms-random-class.directive.js
@@ -0,0 +1,23 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('fuse')
+        .directive('msRandomClass', msRandomClassDirective);
+
+    /** @ngInject */
+    function msRandomClassDirective()
+    {
+        return {
+            restrict: 'A',
+            scope   : {
+                msRandomClass: "="
+            },
+            link    : function (scope, iElement)
+            {
+                var randomClass = scope.msRandomClass[Math.floor(Math.random() * (scope.msRandomClass.length))];
+                iElement.addClass(randomClass);
+            }
+        };
+    }
+})();
