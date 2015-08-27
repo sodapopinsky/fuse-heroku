2b6fc6a6e539752bbc40f4e6f8d441040e6e9041
srcn <sercanyemen@gmail.com>
2015-08-27 15:38:08 +0300

    widgets

--- src/app/main/components/widgets/stat/stat-widgets.controller.js
+++ src/app/main/components/widgets/stat/stat-widgets.controller.js
@@ -13,73 +13,114 @@
 
         // Data
         vm.widget1 = {
-            title: 'WEEKLY TRANSACTIONS',
-            value: 30342,
+            title        : 'WEEKLY TRANSACTIONS',
+            value        : 30342,
             lastWeekValue: 30002,
-            lastWeekDiff: '+ 1,12%',
-            detail: 'This is the back side. You can show detailed information here.'
+            lastWeekDiff : '+ 1,12%',
+            detail       : 'This is the back side. You can show detailed information here.'
         };
 
         vm.widget2 = {
-            title: 'SALES QUOTA',
-            value: 40,
+            title        : 'SALES QUOTA',
+            value        : 40,
             lastWeekValue: 85,
-            lastWeekDiff: '- 45%',
-            detail: 'This is the back side. You can show detailed information here.'
+            lastWeekDiff : '- 45%',
+            detail       : 'This is the back side. You can show detailed information here.'
         };
 
         vm.widget3 = {
-            title: 'BOUNCE RATE',
-            value: 80,
+            title : 'BOUNCE RATE',
+            value : 80,
             detail: 'This is the back side. You can show detailed information here.'
         };
 
         vm.widget4 = {
-            title: 'STOCK COUNT',
-            value: 5583,
+            title        : 'STOCK COUNT',
+            value        : 5583,
             lastWeekValue: 5583,
-            lastWeekDiff: '- 0%',
-            detail: 'This is the back side. You can show detailed information here.'
+            lastWeekDiff : '- 0%',
+            detail       : 'This is the back side. You can show detailed information here.'
         };
 
         vm.widget5 = {
             title: 'USERS ONLINE',
-            value: 658,
+            value: 658
         };
 
         vm.widget6 = {
             title: 'WASTELANDERS',
-            value: 358,
+            value: 358
         };
 
         vm.widget7 = {
             title: 'VAULTS SEALED',
-            value: 24,
+            value: 24
         };
 
         vm.widget8 = {
             title: 'VAULTS OPEN',
-            value: 62,
+            value: 62
         };
 
         vm.widget9 = {
             title: 'SONGS',
-            value: 210,
+            value: 210
         };
 
         vm.widget10 = {
             title: 'VIDEOS',
-            value: 54,
+            value: 54
         };
 
         vm.widget11 = {
             title: 'DOCUMENTS',
-            value: 1252,
+            value: 1252
         };
 
         vm.widget12 = {
-            title: 'June 27, Saturday',
-            value: 9963,
+            date       : 'June 27, Saturday',
+            temperature: 32,
+            event      : 'Sunny',
+            icon       : 'icon-sun',
+            location   : 'New York, NY',
+            detail     : [
+                {
+                    day        : 'Sun',
+                    icon       : 'icon-rainy2',
+                    temperature: 24,
+                    event      : 'Rainy'
+                },
+                {
+                    day        : 'Mon',
+                    icon       : 'icon-rainy',
+                    temperature: 23,
+                    event      : 'Rainy'
+                },
+                {
+                    day        : 'Tue',
+                    icon       : 'icon-cloud3',
+                    temperature: 29,
+                    event      : 'Overcast'
+                },
+                {
+                    day        : 'Wed',
+                    icon       : 'icon-sun',
+                    temperature: 28,
+                    event      : 'Sunny'
+                },
+                {
+                    day        : 'Thu',
+                    icon       : 'icon-sun',
+                    temperature: 31,
+                    event      : 'Sunny'
+                }
+            ]
+        };
+
+        vm.widget13 = {
+            title        : 'AVAILABLE SPACE',
+            value        : 83,
+            detail       : 'This is the back side. You can show detailed information here.'
         };
 
         // Methods
