17638cc3fe317c430f59f043343cc9e9f1e77db6
srcn <sercanyemen@gmail.com>
2015-08-29 11:55:29 +0300

    chart widgets..

--- src/app/main/components/widgets/chart/chart-widgets.controller.js
+++ src/app/main/components/widgets/chart/chart-widgets.controller.js
@@ -14,12 +14,59 @@
         // Data
         vm.widget1 = {
             title: 'CPU USAGE',
-            value: 200
+            value: 200,
+            chart: {
+                columns: [
+                    {
+                        id    : 'CPU',
+                        name  : 'CPU',
+                        color : 'white',
+                        values: '30,200,100,400,150,250',
+                        type  : 'spline'
+                    }
+                ]
+            }
         };
 
         vm.widget2 = {
             title   : 'IO RATE',
-            subtitle: 'Showing last 5 hours'
+            subtitle: 'Showing last 5 hours',
+            chart   : {
+                columns: [
+                    {
+                        id    : 'Input',
+                        name  : 'Input',
+                        color : 'green',
+                        values: '30,75,290,400,150,250',
+                        type  : 'spline'
+                    },
+                    {
+                        id    : 'Output',
+                        name  : 'Output',
+                        color : 'blue',
+                        values: '500,300,120,600,50,80',
+                        type  : 'spline'
+                    }
+                ]
+            }
+        };
+
+        vm.widget3 = {
+            title        : 'WEEKLY VISITORS',
+            value        : 30342,
+            lastWeekValue: 30002,
+            lastWeekDiff : "1,12%",
+            chart        : {
+                columns: [
+                    {
+                        id    : 'Visitors',
+                        name  : 'Visitors',
+                        color : 'steelblue',
+                        values: '30,75,290,400,150,250,75,210,125,92,30,75,290,400',
+                        type  : 'bar'
+                    }
+                ]
+            }
         };
 
         // Methods
