ef280bef4f9d853feb7f62b86f3d0dd102305e3a
srcn <sercanyemen@gmail.com>
2015-08-31 14:08:29 +0300

    auth page states

--- src/app/main/pages/auth/forgot-password/forgot-password.module.js
+++ src/app/main/pages/auth/forgot-password/forgot-password.module.js
@@ -3,6 +3,26 @@
     'use strict';
 
     angular
-        .module('app.pages.auth.forgot-password', []);
+        .module('app.pages.auth.forgot-password', [])
+        .config(config);
+
+    /** @ngInject */
+    function config($stateProvider, $translatePartialLoaderProvider)
+    {
+        $stateProvider.state('app.pages_auth_forgot-password', {
+            url  : '/pages/auth/forgot-password',
+            views: {
+                'main@'                  : {
+                    templateUrl: 'app/core/layouts/basic.html'
+                },
+                'content@app.pages_auth_forgot-password': {
+                    templateUrl: 'app/main/pages/auth/forgot-password/forgot-password.html',
+                    controller : 'ForgotPasswordController as vm'
+                }
+            }
+        });
+
+        $translatePartialLoaderProvider.addPart('app/main/pages/auth/forgot-password');
+    }
 
 })();
