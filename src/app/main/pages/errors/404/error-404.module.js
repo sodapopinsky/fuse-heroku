577a47c2dc78930f58104df711aaa407d0714fec
mustafahlvc <mustafahlvc@gmail.com>
2015-08-31 16:25:43 +0300

    Localization for 404 Page

--- src/app/main/pages/errors/404/error-404.module.js
+++ src/app/main/pages/errors/404/error-404.module.js
@@ -7,7 +7,7 @@
         .config(config);
 
     /** @ngInject */
-    function config($stateProvider)
+    function config($stateProvider, $translatePartialLoaderProvider)
     {
         $stateProvider.state('app.pages_errors_error-404', {
             url  : '/pages/errors/error-404',
@@ -21,6 +21,9 @@
                 }
             }
         });
+
+        $translatePartialLoaderProvider.addPart('app/main/pages/errors/404');
+
     }
 
 })();
