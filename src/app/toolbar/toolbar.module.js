9ecb5b17ae4a44c9d64f344e45e88114b2080dd5
mustafahlvc <mustafahlvc@gmail.com>
2015-09-02 00:13:45 +0300

    Localization for toolbar.

--- src/app/toolbar/toolbar.module.js
+++ src/app/toolbar/toolbar.module.js
@@ -3,6 +3,12 @@
     'use strict';
 
     angular
-        .module('app.toolbar', []);
+        .module('app.toolbar', [])
+        .config(config);
 
+    /** @ngInject */
+    function config($translatePartialLoaderProvider)
+    {
+        $translatePartialLoaderProvider.addPart('app/toolbar');
+    }
 })();
