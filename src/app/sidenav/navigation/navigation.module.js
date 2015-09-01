5c44849ac494581a304de91461f6c1a46f45a9b5
mustafahlvc <mustafahlvc@gmail.com>
2015-09-02 00:43:01 +0300

    Localization for Navigation.

--- src/app/sidenav/navigation/navigation.module.js
+++ src/app/sidenav/navigation/navigation.module.js
@@ -3,6 +3,13 @@
     'use strict';
 
     angular
-        .module('app.navigation', []);
+        .module('app.navigation', [])
+        .config(config);
+
+    /** @ngInject */
+    function config($translatePartialLoaderProvider)
+    {
+        $translatePartialLoaderProvider.addPart('app/sidenav/navigation');
+    }
 
 })();
