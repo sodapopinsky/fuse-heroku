71d9870aaa915324d89d432405378ef970d58137
srcn <sercanyemen@gmail.com>
2015-09-01 10:22:00 +0300

    chartjs page tweak

--- src/app/main/components/charts/chartJs/chartJs.controller.js
+++ src/app/main/components/charts/chartJs/chartJs.controller.js
@@ -12,17 +12,14 @@
 
         // Data
         vm.themes = fuseTheming.themes;
+
         vm.lineChart = {
-            labels : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            series : ['Series A', 'Series B'],
-            data   : [
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            series: ['Series A', 'Series B'],
+            data  : [
                 [65, 59, 80, 81, 56, 55, 40],
                 [28, 48, 40, 19, 86, 27, 90]
-            ],
-            onClick: function (points, evt)
-            {
-                console.log(points, evt);
-            }
+            ]
         };
 
         vm.barChart = {
