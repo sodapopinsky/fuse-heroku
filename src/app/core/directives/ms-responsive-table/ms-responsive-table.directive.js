1aeb749796d2bddeb6553f08644f22c9fcee1977
srcn <sercanyemen@gmail.com>
2015-09-02 20:55:28 +0300

    ms-responsive-table directive

---
+++ src/app/core/directives/ms-responsive-table/ms-responsive-table.directive.js
@@ -0,0 +1,32 @@
+(function ()
+{
+    'use strict';
+
+    angular.module('fuse')
+        .directive('msResponsiveTable', msResponsiveTableDirective);
+
+    /** @ngInject */
+    function msResponsiveTableDirective()
+    {
+        return {
+            restrict  : 'A',
+            transclude: 'element',
+            compile   : function (tElement)
+            {
+                var wrapper = angular.element('<div class="ms-responsive-table-wrapper"></div>');
+                tElement.after(wrapper);
+
+                return function postLink(scope, iElement, iAttrs, ctrl, transcludeFn)
+                {
+                    // Custom transclusion
+                    transcludeFn(function (clone)
+                    {
+                        wrapper.append(clone);
+                    });
+
+                    //////////
+                };
+            }
+        };
+    }
+})();
