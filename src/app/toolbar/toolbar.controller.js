6318a3e4efbb1421b2f06706992c154c0366246a
mustafahlvc <mustafahlvc@gmail.com>
2015-09-27 12:50:39 +0300

    IMPROVEMENT: User menu improvements

--- src/app/toolbar/toolbar.controller.js
+++ src/app/toolbar/toolbar.controller.js
@@ -18,6 +18,37 @@
         // Methods
         vm.toggleSidenav = toggleSidenav;
         vm.toggleNavigationSidenavFold = toggleNavigationSidenavFold;
+        vm.logout = logout;
+        vm.setUserStatus = setUserStatus;
+        vm.userStatusOptions = [
+            {
+                'title': 'Online',
+                'icon' : 'icon-check-circle',
+                'color': '#7DBC00'
+            },
+            {
+                'title': 'Away',
+                'icon' : 'icon-clock',
+                'color': '#F9CE15'
+            },
+            {
+                'title': 'Do not Disturb',
+                'icon' : 'icon-minus-circle',
+                'color': '#EB0615'
+            },
+            {
+                'title': 'Invisible',
+                'icon' : 'icon-checkbox-blank-circle-outline',
+                'color': '#d7d7d7'
+            },
+            {
+                'title': 'Offline',
+                'icon' : 'icon-checkbox-blank-circle-outline',
+                'color': '#616161'
+            }
+        ];
+
+        vm.userStatus = vm.userStatusOptions[0];
 
         //////////
 
@@ -39,6 +70,23 @@
             event.preventDefault();
 
             msNavFoldService.toggleFold();
+        }
+
+        /**
+         * Sets User Status
+         * @param status
+         */
+        function setUserStatus(status)
+        {
+            vm.userStatus = status;
+        }
+
+        /**
+         * Logout Function
+         */
+        function logout()
+        {
+            console.log('Logout Clicked');
         }
     }
 
