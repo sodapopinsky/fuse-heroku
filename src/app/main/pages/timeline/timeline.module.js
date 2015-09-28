bd9c4c622c0974c59a9cd50bba3f23cf3418ebf5
mustafahlvc <mustafahlvc@gmail.com>
2015-09-29 00:33:58 +0300

    IMPROVEMENT: Timeline Variations.

--- src/app/main/pages/timeline/timeline.module.js
+++ src/app/main/pages/timeline/timeline.module.js
@@ -9,21 +9,52 @@
     /** @ngInject */
     function config($stateProvider)
     {
-        $stateProvider.state('app.pages_timeline', {
-            url    : '/pages/timeline',
-            views  : {
-                'content@app': {
-                    templateUrl: 'app/main/pages/timeline/timeline.html',
-                    controller : 'TimelineController as vm'
+        $stateProvider
+            .state('app.pages_timeline', {
+                url    : '/pages/timeline',
+                views  : {
+                    'content@app': {
+                        templateUrl: 'app/main/pages/timeline/timeline.html',
+                        controller : 'TimelineController as vm'
+                    }
+                },
+                resolve: {
+                    Timeline: function (apiResolver)
+                    {
+                        return apiResolver.resolve('timeline@get');
+                    }
                 }
-            },
-            resolve: {
-                Timeline: function (apiResolver)
-                {
-                    return apiResolver.resolve('timeline@get');
+            })
+            .state('app.pages_timeline_left', {
+                url    : '/pages/timeline-left',
+                views  : {
+                    'content@app': {
+                        templateUrl: 'app/main/pages/timeline/timeline-left.html',
+                        controller : 'TimelineController as vm'
+                    }
+                },
+                resolve: {
+                    Timeline: function (apiResolver)
+                    {
+                        return apiResolver.resolve('timeline@get');
+                    }
                 }
-            }
-        });
+            })
+            .state('app.pages_timeline_right', {
+                url    : '/pages/timeline-right',
+                views  : {
+                    'content@app': {
+                        templateUrl: 'app/main/pages/timeline/timeline-right.html',
+                        controller : 'TimelineController as vm'
+                    }
+                },
+                resolve: {
+                    Timeline: function (apiResolver)
+                    {
+                        return apiResolver.resolve('timeline@get');
+                    }
+                }
+            });
     }
 
 })();
