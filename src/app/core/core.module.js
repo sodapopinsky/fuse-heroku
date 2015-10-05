de52fcd748d7af9f5d213e91f8435b8477de6d83
Sercan Yemen <sercanyemen@gmail.com>
2015-10-05 09:08:07 +0300

    formatting tweaks

--- src/app/core/core.module.js
+++ src/app/core/core.module.js
@@ -4,25 +4,25 @@
 
     angular
         .module('app.core',
-        [
-            'ngAnimate',
-            'ngAria',
-            'ngCookies',
-            'ngMessages',
-            'ngResource',
-            'ngRoute',
-            'ngSanitize',
-            'ngTouch',
-            'ngMaterial',
-            'angular-chartist',
-            'chart.js',
-            'datatables',
-            'gridshore.c3js.chart',
-            'nvd3',
-            'pascalprecht.translate',
-            'timer',
-            'ui.router',
-            'uiGmapgoogle-maps',
-            'textAngular'
-        ]);
+            [
+                'ngAnimate',
+                'ngAria',
+                'ngCookies',
+                'ngMessages',
+                'ngResource',
+                'ngRoute',
+                'ngSanitize',
+                'ngTouch',
+                'ngMaterial',
+                'angular-chartist',
+                'chart.js',
+                'datatables',
+                'gridshore.c3js.chart',
+                'nvd3',
+                'pascalprecht.translate',
+                'timer',
+                'ui.router',
+                'uiGmapgoogle-maps',
+                'textAngular'
+            ]);
 })();
