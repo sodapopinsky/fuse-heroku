68c8085a3771c90ab0c5b81b77f48558c4c5b3e1
Sercan Yemen <sercanyemen@gmail.com>
2015-10-07 14:16:26 +0300

    todo search & filtering

--- src/app/main/apps/todo/todo.controller.js
+++ src/app/main/apps/todo/todo.controller.js
@@ -3,57 +3,93 @@
     'use strict';
 
     angular.module('app.todo')
-        .controller('TodoController', TodoController)
-        .filter('filterTags', filterTags);
+        .controller('TodoController', TodoController);
 
     /** @ngInject */
-    function TodoController($document, $mdDialog, $mdSidenav, Tasks, Tags)
+    function TodoController($document, $filter, $mdDialog, $mdSidenav, Tasks, Tags)
     {
         var vm = this;
 
         // Data
         vm.checked = [];
         vm.colors = ['blue', 'blue-grey', 'orange', 'pink', 'purple'];
-
         vm.projects = {
             'creapond'    : 'Project Creapond',
             'withinpixels': 'Project Withinpixels'
         };
-
         vm.selectedFilter = {
             filter: 'Start Date',
             next  : 'Next 3 days'
         };
-
         vm.selectedProject = 'creapond';
-
         vm.tasks = Tasks.data;
         vm.tags = Tags.data;
-        vm.tagFilter = [];
+
+        // Tasks will be filtered against these models
+        vm.taskFilters = {
+            search: undefined,
+            tags  : []
+        };
 
         // Methods
+        vm.filterTask = filterTask;
         vm.openTaskDialog = openTaskDialog;
         vm.toggleCheck = toggleCheck;
         vm.toggleSidenav = toggleSidenav;
+        vm.toggleTagFilter = toggleTagFilter;
+        vm.isTagFilterExists = isTagFilterExists;
 
         //////////
 
-        vm.toggle = function (item, list)
+        function filterTask(task)
         {
-            var idx = list.indexOf(item);
-            if ( idx > -1 )
+            var match = true,
+                result = [];
+
+            if ( vm.taskFilters.search )
             {
-                list.splice(idx, 1);
+                result = $filter('filter')([task.title, task.notes, task.tags], vm.taskFilters.search);
+                match = (match && result.length > 0);
+            }
+
+            if ( vm.taskFilters.tags.length > 0 )
+            {
+                result = $filter('filterSingleByTags')(task.tags, vm.taskFilters.tags);
+                match = (match && result.length > 0);
+            }
+
+            return match;
+        }
+
+        /**
+         * Toggles tag filter
+         *
+         * @param tag
+         */
+        function toggleTagFilter(tag)
+        {
+            var i = vm.taskFilters.tags.indexOf(tag);
+
+            if ( i > -1 )
+            {
+                vm.taskFilters.tags.splice(i, 1);
             }
             else
             {
-                list.push(item);
+                vm.taskFilters.tags.push(tag);
             }
-        };
-        vm.exists = function (item, list)
+        }
+
+        /**
+         * Returns if tag exists in the tagsFilter
+         *
+         * @param tag
+         * @returns {boolean}
+         */
+        function isTagFilterExists(tag)
         {
-            return list.indexOf(item) > -1;
-        };
+            return vm.taskFilters.tags.indexOf(tag) > -1;
+        }
 
         /**
          * Open new task dialog
@@ -99,40 +135,4 @@
         }
     }
 
-    /**
-     * Tag Filter
-     * @returns {Function}
-     */
-    function filterTags()
-    {
-        return function (items, tagFilter)
-        {
-            if ( tagFilter.length <= 0 )
-            {
-                return items;
-            }
-            var filtered = [];
-
-            for ( var i = 0; i < items.length; i++ )
-            {
-                var item = items[i];
-
-                for ( var j = 0; j < tagFilter.length; j++ )
-                {
-                    var filter = tagFilter[j];
-
-                    for ( var k = 0; k < item.tags.length; k++ )
-                    {
-                        var tag = item.tags[k];
-
-                        if ( tag.title === filter )
-                        {
-                            filtered.push(item);
-                        }
-                    }
-                }
-            }
-            return filtered;
-        };
-    }
 })();
