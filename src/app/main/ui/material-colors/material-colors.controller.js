80f69e2452ec3cfac62f24b687ac6acea1140c14
Sercan Yemen <sercanyemen@gmail.com>
2015-10-11 09:29:11 +0300

    mterial-colors page tweaks and formating fixes

--- src/app/main/ui/material-colors/material-colors.controller.js
+++ src/app/main/ui/material-colors/material-colors.controller.js
@@ -2,24 +2,20 @@
 {
     'use strict';
 
-    angular.module('app.ui.material-colors')
+    angular
+        .module('app.ui.material-colors')
         .controller('MaterialColorsController', MaterialColorsController);
 
     /** @ngInject */
-    function MaterialColorsController($mdDialog, $document, $mdColorPalette)
+    function MaterialColorsController($mdColorPalette)
     {
         var vm = this;
+
         // Data
         vm.palettes = $mdColorPalette;
-        //console.log(vm.palettes);
 
         // Methods
 
         //////////
-
-
-
     }
 })();
-
-
