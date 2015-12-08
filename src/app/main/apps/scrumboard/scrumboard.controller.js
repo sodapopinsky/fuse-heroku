d2cf8769ef2eba50d8a327ea6805d74d13988c0d
Sercan Yemen <sercanyemen@gmail.com>
2015-12-08 18:03:56 +0200

    IE fix (scrumboard) calculate list-content max-height with jquery + added .list-footer to access the footer easily

--- src/app/main/apps/scrumboard/scrumboard.controller.js
+++ src/app/main/apps/scrumboard/scrumboard.controller.js
@@ -25,6 +25,47 @@
 
         ////////
 
+        init();
+
+        /**
+         * Initialize
+         */
+        function init()
+        {
+            // IE list-content max-height hack
+            if ( angular.element('html').hasClass('explorer') )
+            {
+                // Calculate the height for the first time
+                calculateListContentHeight();
+
+                // Attach calculateListContentHeight function to window resize
+                $window.onresize = function ()
+                {
+                    calculateListContentHeight();
+                }
+            }
+        }
+
+        /**
+         * IE ONLY
+         * Calculate the list-content height
+         * IE ONLY
+         */
+        function calculateListContentHeight()
+        {
+            // Get the required heights for calculations
+            var listWrapperEl = angular.element('#board .list-wrapper').first(),
+                listWrapperElHeight = listWrapperEl.height(),
+                listHeaderElHeight = listWrapperEl.find('.list-header').height(),
+                listFooterElHeight = listWrapperEl.find('.list-footer').height();
+
+            // Calculate the max height
+            var maxHeight = listWrapperElHeight - listHeaderElHeight - listFooterElHeight;
+
+            // Add the max height
+            angular.element('#board .list-content').css({'max-height': maxHeight});
+        }
+
         /**
          * Update Board Uri
          *
