75763361a5e094a0fe05cd6453f4def79a88a323
Sercan Yemen <sercanyemen@gmail.com>
2015-12-28 10:59:58 +0200

    (print & invoice) added new "page-break" class to break pages on print + "printable" body class to make sure page-break will work + added sample usage to the invoice page

--- src/app/main/pages/invoice/invoice.module.js
+++ src/app/main/pages/invoice/invoice.module.js
@@ -24,7 +24,7 @@
                     return apiResolver.resolve('invoice@get');
                 }
             },
-            bodyClass: 'invoice'
+            bodyClass: 'invoice printable'
         });
 
         // Translation
