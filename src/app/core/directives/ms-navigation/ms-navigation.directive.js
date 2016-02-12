57dda1faf9cde8a61ae0b0c0a54b3605aa0bd768
Sercan Yemen <sercanyemen@gmail.com>
2016-02-12 19:13:41 +0200

    (ms-navigation) clear navigation function, Closes #55

--- src/app/core/directives/ms-navigation/ms-navigation.directive.js
+++ src/app/core/directives/ms-navigation/ms-navigation.directive.js
@@ -297,6 +297,7 @@
                 saveItem           : saveItem,
                 deleteItem         : deleteItem,
                 sort               : sortByWeight,
+                clearNavigation    : clearNavigation,
                 setActiveItem      : setActiveItem,
                 getActiveItem      : getActiveItem,
                 getNavigationObject: getNavigationObject,
@@ -311,6 +312,21 @@
             return service;
 
             //////////
+
+            /**
+             * Clear the entire navigation
+             */
+            function clearNavigation()
+            {
+                // Clear the navigation array
+                navigation = [];
+
+                // Clear the vm.navigation from main controller
+                if ( navigationScope )
+                {
+                    navigationScope.vm.navigation = [];
+                }
+            }
 
             /**
              * Set active item
