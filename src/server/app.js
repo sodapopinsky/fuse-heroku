7e646647e02714ad7eeb185b834195df0ee48217
sodapopinsky <Nick@Nicholass-iMac.local>
2016-07-03 19:21:38 -0500

    f

--- src/server/app.js
+++ src/server/app.js
@@ -11,9 +11,9 @@ var path = require('path');
 switch (environment) {
     case 'production':
         console.log('production');
-        app.set('baseDir',path.join(__dirname, '../../../dist'));
-        app.use('/', express.static(path.join(__dirname, '../../../dist')));
-        console.log(path.join(__dirname, '../../../dist'));
+        app.set('baseDir',path.join(__dirname, '../../../dist/'));
+        app.use('/', express.static(path.join(__dirname, '../../../dist/')));
+        console.log(path.join(__dirname, '../../../dist/'));
         break;
     default:
         console.log('dev');
